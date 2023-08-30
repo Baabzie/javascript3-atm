444224f682f2bdf409ec74876dcebe2a15b8f93d
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2023-08-30 09:41:34 +0200

    första push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import ATM from './components/ATM';
 
 function App() {
+  const [showATM, setShowATM] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ATM-App</h1>
+      <button onClick={() => {setShowATM((prevState) => !prevState)}}>{showATM ? "Hide ATM" : "Show ATM"}</button>
+      {showATM && <ATM/>}
     </div>
   );
 }
