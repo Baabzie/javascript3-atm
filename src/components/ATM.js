2417c9212d77d35f562c27afe2b36d6bcaf0a1cb
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2023-08-30 13:49:57 +0200

    done MVP

--- src/components/ATM.js
+++ src/components/ATM.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
 
 export default function ATM() {
 
@@ -34,22 +34,60 @@ export default function ATM() {
     ];
 
     let [currentCurrency, setCurrentCurrency] = useState(currencies[0]);
+    let [withdrawAmount, setWithdrawAmount] = useState(0);
+    let [balance, setBalance] = useState(0);
+
+    useEffect(() => {
+        console.log("Booting up ATM...ATM is ready!")
+        return () => {
+            (console.log("ATM shutting down…"))
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log("Current amount: " + balance/currentCurrency.dollarValue + " " + currentCurrency.name)
+    }, [balance])
 
     return (
         <>
+            <h2>Your balance: {!currentCurrency.symbolPlacementAfter && <>{currentCurrency.symbol}</>} {Math.round((balance/currentCurrency.dollarValue)*100)/100} {currentCurrency.symbolPlacementAfter && <>{currentCurrency.symbol}</>} </h2>
+            <input type="number" id="depositInput"></input>
+            <br/>
+            <button onClick={() => {
+                setBalance((prevState) => prevState + (document.getElementById("depositInput").valueAsNumber)*currentCurrency.dollarValue)
+            }}>Deposit {currentCurrency.name}</button>
+
+            <h2>
+                Withdraw: {!currentCurrency.symbolPlacementAfter && <>{currentCurrency.symbol}</>} {/* If the symbol of the currency is suppose to be showed before the amount (Example: $50) */}
+                {withdrawAmount} {/* Amount to be withdrawned */}
+                {currentCurrency.symbolPlacementAfter && <>{currentCurrency.symbol}</>} {/* If the symbol of the currency is suppose to be showed after the amount (Example: 500kr) */}
+            </h2>
+            
             {currentCurrency.notes.map((note) => {
                 return (
-                    <button>{!currentCurrency.symbolPlacementAfter && <>{currentCurrency.symbol}</>}{note}{currentCurrency.symbolPlacementAfter && <>{currentCurrency.symbol}</>}</button>
+                    <button onClick={() => {
+                        setWithdrawAmount((prevState) => prevState + note)}}>
+                        {!currentCurrency.symbolPlacementAfter && <>{currentCurrency.symbol}</>} {/* If the symbol of the currency is suppose to be showed before the amount (Example: $50) */}
+                        {note} {/* Value on button to be added to "withdrawAmount" */}
+                        {currentCurrency.symbolPlacementAfter && <>{currentCurrency.symbol}</>} {/* If the symbol of the currency is suppose to be showed after the amount (Example: 500kr) */}
+                    </button>
                 )
             })}
             <br/>
+            <button disabled={withdrawAmount === 0} onClick={() => { setWithdrawAmount(0);
+            {withdrawAmount*currentCurrency.dollarValue < balance ? setBalance((prevState) => prevState - withdrawAmount*currentCurrency.dollarValue) : console.log("Not enough balance on account.")}}}>
+                Withdraw Money</button>
+            <br/>
             {currencies.map((currency, i) => {
                 return (
-                    currency.name !== currentCurrency.name && <button onClick={() => {setCurrentCurrency(currencies[i])}}>{currency.name}</button>
+                    <button disabled={currency.name === currentCurrency.name} onClick={() => {
+                        setCurrentCurrency(currencies[i]); /* Change active currency to the one of the button */
+                        console.log("Chosen corrency: " + currencies[i].name)
+                        setWithdrawAmount(0)}}> {/* Set "withdrawAmount" to 0. */}
+                        {currency.name}
+                    </button>
                 )
             })}
-            <p>{currentCurrency.name}</p>
-            <p>{currentCurrency.symbol}</p>
         </>
     )
 }
